f96dffa3b4d47babcbbbe7d83290d6787a342bed
Martín <magadiflo@gmail.com>
2022-01-14 19:51:17 -0500

    Configurando Datasource en servidor WildFly y persistencia JPA en el módulo ejb

---
+++ webapp-ear-ejb/src/main/java/org/magadiflo/webapp/ear/ejb/ProducerResources.java
@@ -0,0 +1,30 @@
+package org.magadiflo.webapp.ear.ejb;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
+
+@ApplicationScoped
+public class ProducerResources {
+
+    @PersistenceUnit(name = "HibernateJpaPU")
+    private EntityManagerFactory emf;
+
+    @Produces
+    @RequestScoped
+    private EntityManager beanEntityManager() {
+        return this.emf.createEntityManager();
+    }
+
+    public void close(@Disposes EntityManager entityManager){
+        if(entityManager.isOpen()){
+             entityManager.close();
+            System.out.println(">>>>>>>>>>> ¡Cerrando conexion de EntityManager!");
+        }
+    }
+
+}
